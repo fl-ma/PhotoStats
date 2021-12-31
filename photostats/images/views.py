7f1b4279a0ac985fcb968e583f4be24810d31500
fl2ma <35446987+fl-ma@users.noreply.github.com>
2021-12-31 18:07:16 +0800

    image list view as html table

--- photostats/images/views.py
+++ photostats/images/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Image
 
 # Create your views here.
 def list(request):
     image_list = Image.objects.all()
-    output = ', '.join([i.filename for i in image_list])
-    return HttpResponse(output)
+
+    template = loader.get_template('images/index.html')
+    context = {
+        'image_list': image_list,
+    }
+    return HttpResponse(template.render(context, request))

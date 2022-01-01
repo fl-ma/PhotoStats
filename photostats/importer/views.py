1197f48d7cf14d437aa1234298aa7aafb634d8cf
fl2ma <35446987+fl-ma@users.noreply.github.com>
2022-01-01 17:39:51 +0800

    distinguish between buttons

--- photostats/importer/views.py
+++ photostats/importer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError
 from django.template import loader
 
 def index(request):
@@ -7,14 +7,27 @@ def index(request):
     # return HttpResponse("Hello, world. You're at the importer action.")
 
     # image_list = Image.objects.all()
-
-    print("Index")
-
     template = loader.get_template('importer/index.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
 
 def action(request):
-    print("action")
-    return HttpResponse("Congrats, you clicked")
+    
+    #determine button that was clicked
+    button = request.POST.get('list')
+    
+    if not button:
+        button = request.POST.get('delete')
+        
+    if not button:
+        button = request.POST.get('import')
+    
+    if not button:
+        text = "Button clicked is unknown: " + button
+        return HttpResponseServerError(text)
+    
+    else:
+        text = "Congrats, you clicked: " + button
+        
+        return HttpResponse(text)
